eebdb8f116ebf21a68a702dd3251a9df14fa59dc
Deepikaik <deepikabhagavati2001@gmail.com>
2024-08-20 13:29:00 +0530

    'js'

---
+++ OOP - Copy (2)/ONE/seven.java
@@ -0,0 +1,8 @@
+class test{
+    test(){
+        system.out.print("special");
+    }
+    public static void main(string[] args){
+        new test();
+    }
+}
---
+++ OOP - Copy/ONE/two.js
@@ -0,0 +1,9 @@
+class Account{               //let or var or function is not valid in the class////only vlass members are allowed
+ minBal=500;
+ openAccount() {}                 //class contains properties and methods
+}
+let r= new Account()
+let s= new Account()
+console.log(r)
+console.log(s)
+
